d6f3c462b1b9f79825b8c91098e04183938b6a14
Jenny <jennybell42@gmail.com>
2021-05-19 13:52:26 +0100

    database connected

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'capybara'
 gem 'rspec'
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'pg'
 
 group :development, :test do
   gem 'rubocop', '0.79.0'
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,7 @@
 feature 'Bookmarks list' do
   scenario 'shows a list of bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
